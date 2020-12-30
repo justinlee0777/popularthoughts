d553bb70b0fecfa61bb5fa5ea81644fb26c7257a
Justin Lee <justinlee@Justins-MacBook-Air.local>
2020-12-29 21:48:36 -0500

    Remove old public.zip artifact on new build

--- graphql-types.ts
+++ graphql-types.ts
@@ -389,8 +389,8 @@ export type File = Node & {
   parent?: Maybe<Node>;
   children: Array<Node>;
   internal: Internal;
-  childMarkdownRemark?: Maybe<MarkdownRemark>;
   childrenNavBarJson?: Maybe<Array<Maybe<NavBarJson>>>;
+  childMarkdownRemark?: Maybe<MarkdownRemark>;
 };
 
 
@@ -597,6 +597,48 @@ export type FileFieldsEnum =
   | 'internal___mediaType'
   | 'internal___owner'
   | 'internal___type'
+  | 'childrenNavBarJson'
+  | 'childrenNavBarJson___id'
+  | 'childrenNavBarJson___parent___id'
+  | 'childrenNavBarJson___parent___parent___id'
+  | 'childrenNavBarJson___parent___parent___children'
+  | 'childrenNavBarJson___parent___children'
+  | 'childrenNavBarJson___parent___children___id'
+  | 'childrenNavBarJson___parent___children___children'
+  | 'childrenNavBarJson___parent___internal___content'
+  | 'childrenNavBarJson___parent___internal___contentDigest'
+  | 'childrenNavBarJson___parent___internal___description'
+  | 'childrenNavBarJson___parent___internal___fieldOwners'
+  | 'childrenNavBarJson___parent___internal___ignoreType'
+  | 'childrenNavBarJson___parent___internal___mediaType'
+  | 'childrenNavBarJson___parent___internal___owner'
+  | 'childrenNavBarJson___parent___internal___type'
+  | 'childrenNavBarJson___children'
+  | 'childrenNavBarJson___children___id'
+  | 'childrenNavBarJson___children___parent___id'
+  | 'childrenNavBarJson___children___parent___children'
+  | 'childrenNavBarJson___children___children'
+  | 'childrenNavBarJson___children___children___id'
+  | 'childrenNavBarJson___children___children___children'
+  | 'childrenNavBarJson___children___internal___content'
+  | 'childrenNavBarJson___children___internal___contentDigest'
+  | 'childrenNavBarJson___children___internal___description'
+  | 'childrenNavBarJson___children___internal___fieldOwners'
+  | 'childrenNavBarJson___children___internal___ignoreType'
+  | 'childrenNavBarJson___children___internal___mediaType'
+  | 'childrenNavBarJson___children___internal___owner'
+  | 'childrenNavBarJson___children___internal___type'
+  | 'childrenNavBarJson___internal___content'
+  | 'childrenNavBarJson___internal___contentDigest'
+  | 'childrenNavBarJson___internal___description'
+  | 'childrenNavBarJson___internal___fieldOwners'
+  | 'childrenNavBarJson___internal___ignoreType'
+  | 'childrenNavBarJson___internal___mediaType'
+  | 'childrenNavBarJson___internal___owner'
+  | 'childrenNavBarJson___internal___type'
+  | 'childrenNavBarJson___label'
+  | 'childrenNavBarJson___value'
+  | 'childrenNavBarJson___markdownCriteria'
   | 'childMarkdownRemark___id'
   | 'childMarkdownRemark___frontmatter___title'
   | 'childMarkdownRemark___frontmatter___slug'
@@ -654,49 +696,7 @@ export type FileFieldsEnum =
   | 'childMarkdownRemark___internal___ignoreType'
   | 'childMarkdownRemark___internal___mediaType'
   | 'childMarkdownRemark___internal___owner'
-  | 'childMarkdownRemark___internal___type'
-  | 'childrenNavBarJson'
-  | 'childrenNavBarJson___id'
-  | 'childrenNavBarJson___parent___id'
-  | 'childrenNavBarJson___parent___parent___id'
-  | 'childrenNavBarJson___parent___parent___children'
-  | 'childrenNavBarJson___parent___children'
-  | 'childrenNavBarJson___parent___children___id'
-  | 'childrenNavBarJson___parent___children___children'
-  | 'childrenNavBarJson___parent___internal___content'
-  | 'childrenNavBarJson___parent___internal___contentDigest'
-  | 'childrenNavBarJson___parent___internal___description'
-  | 'childrenNavBarJson___parent___internal___fieldOwners'
-  | 'childrenNavBarJson___parent___internal___ignoreType'
-  | 'childrenNavBarJson___parent___internal___mediaType'
-  | 'childrenNavBarJson___parent___internal___owner'
-  | 'childrenNavBarJson___parent___internal___type'
-  | 'childrenNavBarJson___children'
-  | 'childrenNavBarJson___children___id'
-  | 'childrenNavBarJson___children___parent___id'
-  | 'childrenNavBarJson___children___parent___children'
-  | 'childrenNavBarJson___children___children'
-  | 'childrenNavBarJson___children___children___id'
-  | 'childrenNavBarJson___children___children___children'
-  | 'childrenNavBarJson___children___internal___content'
-  | 'childrenNavBarJson___children___internal___contentDigest'
-  | 'childrenNavBarJson___children___internal___description'
-  | 'childrenNavBarJson___children___internal___fieldOwners'
-  | 'childrenNavBarJson___children___internal___ignoreType'
-  | 'childrenNavBarJson___children___internal___mediaType'
-  | 'childrenNavBarJson___children___internal___owner'
-  | 'childrenNavBarJson___children___internal___type'
-  | 'childrenNavBarJson___internal___content'
-  | 'childrenNavBarJson___internal___contentDigest'
-  | 'childrenNavBarJson___internal___description'
-  | 'childrenNavBarJson___internal___fieldOwners'
-  | 'childrenNavBarJson___internal___ignoreType'
-  | 'childrenNavBarJson___internal___mediaType'
-  | 'childrenNavBarJson___internal___owner'
-  | 'childrenNavBarJson___internal___type'
-  | 'childrenNavBarJson___label'
-  | 'childrenNavBarJson___value'
-  | 'childrenNavBarJson___markdownCriteria';
+  | 'childMarkdownRemark___internal___type';
 
 export type FileFilterInput = {
   sourceInstanceName?: Maybe<StringQueryOperatorInput>;
@@ -737,8 +737,8 @@ export type FileFilterInput = {
   parent?: Maybe<NodeFilterInput>;
   children?: Maybe<NodeFilterListInput>;
   internal?: Maybe<InternalFilterInput>;
-  childMarkdownRemark?: Maybe<MarkdownRemarkFilterInput>;
   childrenNavBarJson?: Maybe<NavBarJsonFilterListInput>;
+  childMarkdownRemark?: Maybe<MarkdownRemarkFilterInput>;
 };
 
 export type FileGroupConnection = {
@@ -1321,8 +1321,8 @@ export type QueryFileArgs = {
   parent?: Maybe<NodeFilterInput>;
   children?: Maybe<NodeFilterListInput>;
   internal?: Maybe<InternalFilterInput>;
-  childMarkdownRemark?: Maybe<MarkdownRemarkFilterInput>;
   childrenNavBarJson?: Maybe<NavBarJsonFilterListInput>;
+  childMarkdownRemark?: Maybe<MarkdownRemarkFilterInput>;
 };
 
 
@@ -1386,8 +1386,6 @@ export type QueryAllDirectoryArgs = {
 export type QuerySiteArgs = {
   buildTime?: Maybe<DateQueryOperatorInput>;
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>;
-  port?: Maybe<IntQueryOperatorInput>;
-  host?: Maybe<StringQueryOperatorInput>;
   polyfill?: Maybe<BooleanQueryOperatorInput>;
   pathPrefix?: Maybe<StringQueryOperatorInput>;
   id?: Maybe<StringQueryOperatorInput>;
@@ -1519,8 +1517,6 @@ export type QueryAllSitePluginArgs = {
 export type Site = Node & {
   buildTime?: Maybe<Scalars['Date']>;
   siteMetadata?: Maybe<SiteSiteMetadata>;
-  port?: Maybe<Scalars['Int']>;
-  host?: Maybe<Scalars['String']>;
   polyfill?: Maybe<Scalars['Boolean']>;
   pathPrefix?: Maybe<Scalars['String']>;
   id: Scalars['ID'];
@@ -1722,8 +1718,6 @@ export type SiteFieldsEnum =
   | 'buildTime'
   | 'siteMetadata___title'
   | 'siteMetadata___description'
-  | 'port'
-  | 'host'
   | 'polyfill'
   | 'pathPrefix'
   | 'id'
@@ -1816,8 +1810,6 @@ export type SiteFieldsEnum =
 export type SiteFilterInput = {
   buildTime?: Maybe<DateQueryOperatorInput>;
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>;
-  port?: Maybe<IntQueryOperatorInput>;
-  host?: Maybe<StringQueryOperatorInput>;
   polyfill?: Maybe<BooleanQueryOperatorInput>;
   pathPrefix?: Maybe<StringQueryOperatorInput>;
   id?: Maybe<StringQueryOperatorInput>;
