2e4c366bbdc5f281a3231bf20126b35842fa2b50
Justin Lee <justinlee@Justins-MacBook-Air.local>
2021-01-09 13:07:21 -0500

    Adding youtube icon to nav bar images.d.ts configuration to allow importing image files within code

--- src/shared/nav-bar/nav-bar.tsx
+++ src/shared/nav-bar/nav-bar.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'gatsby';
 import React from 'react';
 
+import youtubeIcon from 'assets/youtube-icon.png';
+
 import { NavBarConfig } from './nav-bar.config';
 import './nav-bar.css';
 
@@ -17,6 +19,17 @@ export default function NavBar(config: NavBarConfig): JSX.Element {
 			</Link>
 		);
 	});
+
+	tabs.push(
+		<Link
+			className="link social youtube"
+			key="youtube"
+			target="_blank"
+			to="https://www.youtube.com/channel/UCVmcB2t71rU_J7dfE63a8RQ"
+		>
+			<img src={youtubeIcon} />
+		</Link>
+	);
 
 	return <nav className={config.className}>{tabs}</nav>;
 }
